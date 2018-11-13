33ca99bbed793b63bf84a5b69345dad90834adca
Bruno Campos <brunosopmac@gmail.com>
2018-11-13 17:12:44 -0200

    challenge #4 finished

--- beginner/003/js/main.js
+++ beginner/003/js/main.js
@@ -1,5 +1,18 @@
 /* 
- * Write a JavaScript program to print
- * the contents of the current window.
+ * Write a JavaScript program to get the current date. 
+ * Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
  */
 
+
+const date = new Date();
+
+let day         = date.getDate();
+let month       = date.getMonth()+1;
+let fullYear    = date.getFullYear();
+
+// days and months format
+day = (day < 10) ? `0${day}` : day;
+month = (month < 10) ? `0${month}` : month;
+
+// show the date
+document.querySelector('#date').innerText = `${day}/${month}/${fullYear}, ${day}-${month}-${fullYear}`;
